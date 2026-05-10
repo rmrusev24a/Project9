1331bb84746bafbd0661943ed8eb83197a0b9421
rmrusev24a <rmrusev24a@gmail.com>
2026-05-10 16:57:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import streamlit as st
+import easyocr
 from PIL import Image
-import pytesseract
+import numpy as np
 
 st.title("Скенер за вредни съставки")
 
+st.write("Качи снимка на етикет")
+
 uploaded_file = st.file_uploader(
-    "Качи снимка",
+    "Избери снимка",
     type=["jpg", "jpeg", "png"]
 )
 
@@ -17,25 +20,43 @@ harmful = [
     "Aspartame"
 ]
 
-if uploaded_file:
+if uploaded_file is not None:
 
     image = Image.open(uploaded_file)
 
-    st.image(image)
+    st.image(image, caption="Качена снимка")
 
-    text = pytesseract.image_to_string(image)
+    img = np.array(image)
+
+    st.write("Разпознаване на текст...")
+
+    reader = easyocr.Reader(
+        ['bg', 'en'],
+        gpu=False
+    )
+
+    result = reader.readtext(
+        img,
+        detail=0
+    )
+
+    text = " ".join(result)
 
     st.subheader("Разпознат текст:")
     st.write(text)
 
-    st.subheader("Резултат:")
+    st.subheader("Проверка:")
 
     found = False
 
     for item in harmful:
+
         if item.lower() in text.lower():
+
             st.error(f"Намерено: {item}")
+
             found = True
 
     if not found:
+
         st.success("Няма намерени вредни съставки.")
