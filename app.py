0c591df87f532d104408f54774e72b78e31e5a36
rmrusev24a <rmrusev24a@gmail.com>
2026-05-10 16:56:27 +0300

    Refactor food scanner to use pytesseract for OCR

--- app.py
+++ app.py
@@ -1,54 +1,39 @@
 import streamlit as st
 from PIL import Image
-import numpy as np
-import easyocr
-
-st.set_page_config(page_title="Food Scanner")
+import pytesseract
 
 st.title("Скенер за вредни съставки")
 
-st.write("Качи снимка на етикет.")
-
 uploaded_file = st.file_uploader(
-    "Избери снимка",
-    type=["png", "jpg", "jpeg"]
+    "Качи снимка",
+    type=["jpg", "jpeg", "png"]
 )
 
-# Вредни съставки
 harmful = [
-    "e621",
-    "e102",
-    "e110",
+    "E621",
+    "E102",
+    "E110",
     "палмово масло",
-    "aspartame"
+    "Aspartame"
 ]
 
-if uploaded_file is not None:
+if uploaded_file:
 
     image = Image.open(uploaded_file)
 
-    st.image(image, caption="Качена снимка")
+    st.image(image)
 
-    img = np.array(image)
-
-    st.write("Сканиране...")
-
-    # OCR
-    reader = easyocr.Reader(['bg', 'en'], gpu=False)
-
-    result = reader.readtext(img, detail=0)
-
-    text = " ".join(result)
+    text = pytesseract.image_to_string(image)
 
     st.subheader("Разпознат текст:")
     st.write(text)
 
-    st.subheader("Проверка:")
+    st.subheader("Резултат:")
 
     found = False
 
     for item in harmful:
-        if item in text.lower():
+        if item.lower() in text.lower():
             st.error(f"Намерено: {item}")
             found = True
 
